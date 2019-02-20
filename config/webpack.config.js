63733aff2be07e23294ed83a667020f38e87bc9f
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-20 19:48:03 +0700

    Configure React hot module reloading with React Hot Loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -347,6 +347,8 @@ module.exports = function(webpackEnv) {
                       },
                     },
                   ],
+                  // React Hot Loader
+                  'react-hot-loader/babel',
                 ],
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
