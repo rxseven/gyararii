81b32e9dfe98248876e9929ea0baa8924185697a
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-20 15:18:54 +0700

    Configure react-testing-library

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,6 +1,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
+import 'react-testing-library/cleanup-after-each';
 
 // Configure Enzyme adapter
 configure({ adapter: new Adapter() });
