5a14bd9f935e6a9768a438cacd207537de3bc2e9
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-20 23:23:20 +0700

    Specify root HTML element to the project starting point

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Constants
+import HTML from 'constants/elements/html';
+
 // Entry point component
 import App from 'components/App';
 
 // Utilities
 import * as serviceWorker from 'serviceWorker';
 
+// Get root HTML element with the specified ID
+const root = document.getElementById(HTML.root);
+
+// Validate root element
+if (root == null) {
+  throw new Error('No root element found');
+}
+
 // Render React element into the DOM
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
