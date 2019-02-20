ed6bdcf5e90b5b78f1f3f01baed16fd0b6d1e520
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-20 15:19:24 +0700

    Configure custom Jest matchers to test the state of the DOM

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,5 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-dom/extend-expect';
 import 'jest-enzyme';
 import 'react-testing-library/cleanup-after-each';
 
