17e333cef7727a3adca18284b3f71d4d954c00de
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-20 22:04:36 +0700

    Configure Storybook Info addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import requireContext from 'require-context.macro';
 import { withBackgrounds } from '@storybook/addon-backgrounds';
+import { withInfo } from '@storybook/addon-info';
 import { withOptions } from '@storybook/addon-options';
 import { themes } from '@storybook/components';
 import { addDecorator, configure } from '@storybook/react';
@@ -26,6 +27,65 @@ addDecorator(
     { name: 'Dark', value: '#242424' },
     { name: 'Facebook', value: '#3b5998' }
   ])
+);
+
+// Info addon
+addDecorator(
+  withInfo({
+    header: true,
+    inline: false,
+    source: true,
+    styles: {
+      button: {
+        base: {
+          fontSize: '11px',
+          padding: '4px 7px'
+        },
+        topRight: {
+          borderRadius: '0 0 0 4px'
+        }
+      },
+      header: {
+        body: {
+          marginBottom: '1rem'
+        },
+        h1: {
+          color: '#007bff',
+          fontSize: '18px',
+          fontWeight: '400',
+          marginBottom: '0.75rem'
+        },
+        h2: {
+          color: '#999',
+          fontSize: '14px',
+          marginBottom: '1rem'
+        }
+      },
+      infoBody: {
+        fontSize: '14px'
+      },
+      infoContent: {
+        borderBottom: '1px solid #eee',
+        marginBottom: '1rem',
+        paddingBottom: '1rem'
+      },
+      propTableHead: {
+        fontSize: '14px',
+        fontWeight: '400',
+        margin: '15px 0'
+      },
+      source: {
+        h1: {
+          color: '#007bff',
+          fontSize: '16px',
+          fontWeight: '400',
+          marginBottom: '1rem',
+          marginTop: 0,
+          paddingBottom: '1rem'
+        }
+      }
+    }
+  })
 );
 
 // Wrap each story within a container
