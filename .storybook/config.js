0c0ca7f23811f8d81252f3ccd79c8a6c914020f4
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-20 21:41:41 +0700

    Initialize Storybook configuration

---
+++ .storybook/config.js
@@ -0,0 +1,16 @@
+import requireContext from 'require-context.macro';
+import { addDecorator, configure } from '@storybook/react';
+
+// Storybook styles
+import './styles.scss';
+
+// Import story files
+const req = requireContext('../src', true, /\.stories\.js$/);
+
+// Load all stories
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+// Configure Storybook
+configure(loadStories, module);
