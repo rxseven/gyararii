b4fb8a2500fa230768e7a66d010e59802d102c14
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-20 21:46:49 +0700

    Configure global story wrapper

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import requireContext from 'require-context.macro';
 import { addDecorator, configure } from '@storybook/react';
 
+// Decorators
+import Wrapper from './decorators/Wrapper';
+
 // Storybook styles
 import './styles.scss';
+
+// Wrap each story within a container
+addDecorator(story => <Wrapper>{story()}</Wrapper>);
 
 // Import story files
 const req = requireContext('../src', true, /\.stories\.js$/);
