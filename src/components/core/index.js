3f265899016bf91fda5d697ded7b2ce20e83dfa9
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-22 00:52:02 +0700

    Create Wrapper component

--- src/components/core/index.js
+++ src/components/core/index.js
@@ -4,3 +4,4 @@ export { default as Routes } from './navigation/Routes';
 // Skeleton
 export { default as Router } from './skeleton/Router';
 export { default as Theme } from './skeleton/Theme';
+export { default as Wrapper } from './skeleton/Wrapper';
