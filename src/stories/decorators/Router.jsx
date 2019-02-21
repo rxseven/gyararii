cbbe5baff9ce302185c89b4342dc413b53445447
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-22 00:49:26 +0700

    Remove unnecessary Storybook Router decorator

--- src/stories/decorators/Router.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-
-function Router(story) {
-  return <BrowserRouter>{story()}</BrowserRouter>;
-}
-
-export default Router;
