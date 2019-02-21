b69373fe4e52c8dd0d50c1a3084909ba2becd3f2
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-22 00:49:55 +0700

    Refactor Router component
    - Add missing type cheking with PropTypes
    - Add missing notes a story
    - Fix incorrect smoke test

--- src/components/core/skeleton/Router/__tests__/Router.test.jsx
+++ src/components/core/skeleton/Router/__tests__/Router.test.jsx
@@ -1,11 +1,18 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 
+import mock from 'tests/mock';
 import Router from '../index';
+
+const seed = {
+  props: {
+    children: mock.elements.children
+  }
+};
 
 describe('<Router />', () => {
   // Arrange
-  const component = <Router />;
+  const component = <Router>{seed.props.children}</Router>;
 
   describe('Unit tests', () => {
     it('should render without crashing', () => {
--- src/components/core/skeleton/Router/index.jsx
+++ src/components/core/skeleton/Router/index.jsx
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types';
+import exact from 'prop-types-exact';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { LastLocationProvider } from 'react-router-last-location';
 import ScrollMemory from 'react-router-scroll-memory';
+
+const propTypes = exact({
+  children: PropTypes.element.isRequired
+});
 
 function Router({ children }) {
   return (
@@ -13,5 +19,7 @@ function Router({ children }) {
     </BrowserRouter>
   );
 }
+
+Router.propTypes = propTypes;
 
 export default Router;
