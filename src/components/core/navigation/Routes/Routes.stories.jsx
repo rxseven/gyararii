8b1bd791866b84c82ae161aebbbc594cba6adf97
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-22 00:50:08 +0700

    Refactor Routes component’s story (remove inividual Router decorator)

--- src/components/core/navigation/Routes/Routes.stories.jsx
+++ src/components/core/navigation/Routes/Routes.stories.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Router from 'stories/decorators/Router';
 import Routes from './index';
 
 const title = 'Components/Core/Navigation/Routes';
 const component = <Routes />;
 
-storiesOf(title, module)
-  .addDecorator(Router)
-  .add('default (404)', () => component);
+storiesOf(title, module).add('default (404)', () => component);
