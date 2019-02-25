2ab2d79b46096b335545ccc4551b4180330cbff5
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-25 12:27:28 +0700

    Refactor Modal.Header with functional component

--- src/components/common/base/Modal/Header/index.jsx
+++ src/components/common/base/Modal/Header/index.jsx
@@ -7,30 +7,20 @@ const propTypes = exact({
   onClose: PropTypes.func.isRequired
 });
 
-class Header extends React.Component {
-  handleClose = () => {
-    const { onClose } = this.props;
-
-    onClose();
-  };
-
-  render() {
-    const { children } = this.props;
-
-    return (
-      <div className="modal-header">
-        <h5 className="modal-title">{children}</h5>
-        <button
-          aria-label="Close"
-          className="close"
-          onClick={this.handleClose}
-          type="button"
-        >
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-    );
-  }
+function Header({ children, onClose }) {
+  return (
+    <div className="modal-header">
+      <h5 className="modal-title">{children}</h5>
+      <button
+        aria-label="Close"
+        className="close"
+        onClick={onClose}
+        type="button"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
 }
 
 Header.propTypes = propTypes;
