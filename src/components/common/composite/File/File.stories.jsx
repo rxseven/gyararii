c98bc773b3aa3b1d39276b11c122d0ab2b08a351
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-25 19:01:30 +0700

    Refactor Fire’s stories & add missing required props

--- src/components/common/composite/File/File.stories.jsx
+++ src/components/common/composite/File/File.stories.jsx
@@ -5,57 +5,37 @@ import { storiesOf } from '@storybook/react';
 import File from './index';
 
 const title = 'Components/Common/Composite/File';
-const actions = { onChange: action('onChange') };
+const props = {
+  children: 'Upload',
+  id: 'gallery',
+  onUpload: action('onUpload')
+};
 
 storiesOf(title, module)
-  .add('default', () => <File {...actions}>Upload</File>, {
+  .add('default', () => <File {...props} />, {
     notes: 'Let a user choose one or more files from their device storage.'
   })
-  .add(
-    'with icon',
-    () => (
-      <File {...actions} icon="cloud-upload-alt" id="avatar">
-        Upload
-      </File>
-    ),
-    {
-      notes: {
-        markdown: `Pass icon name (string or array of string). Check out all available icons from [Font Awesome](https://fontawesome.com/icons?d=gallery).
+  .add('with icon', () => <File {...props} icon="cloud-upload-alt" />, {
+    notes: {
+      markdown: `Pass icon name (string or array of string). Check out all available icons from [Font Awesome](https://fontawesome.com/icons?d=gallery).
         `
-      }
     }
-  )
+  })
   .add(
     'disabled',
-    () => (
-      <File {...actions} icon="cloud-upload-alt" id="avatar" isLoading>
-        Upload
-      </File>
-    ),
+    () => <File {...props} icon="cloud-upload-alt" isLoading />,
     {
       notes: 'Disable a button while a network request is being submitted.'
     }
   )
   .add(
     'with spinner',
-    () => (
-      <File {...actions} id="avatar" isLoading spinner>
-        Upload
-      </File>
-    ),
+    () => <File {...props} id="avatar" isLoading spinner />,
     {
       notes:
         'Disable a button and show a spinner while a network request is being submitted.'
     }
   )
-  .add(
-    'multiple',
-    () => (
-      <File {...actions} id="images" multiple>
-        Upload multiple files
-      </File>
-    ),
-    {
-      notes: 'To allow multiple files to be selected.'
-    }
-  );
+  .add('multiple', () => <File {...props} id="images" multiple />, {
+    notes: 'To allow multiple files to be selected.'
+  });
