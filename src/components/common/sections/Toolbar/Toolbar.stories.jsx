cd026518d7dd8754d9e63befc20f5a1a7a4c72f5
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-25 14:57:00 +0700

    Create Toolbar component

---
+++ src/components/common/sections/Toolbar/Toolbar.stories.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import notes from 'stories/shared/notes';
+import Toolbar from './index';
+
+const title = 'Components/Common/Sections/Toolbar';
+const props = { node: React.createRef() };
+
+storiesOf(title, module)
+  .add('default', () => <Toolbar {...props}>Content</Toolbar>, {
+    notes: notes.minimal
+  })
+  .add(
+    'loading',
+    () => (
+      <Toolbar {...props} isLoading>
+        Content
+      </Toolbar>
+    ),
+    {
+      notes:
+        'A component is invisible while a network request is being submitted.'
+    }
+  );
