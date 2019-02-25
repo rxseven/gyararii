27f78ec92d25ed1f28a7af568a2c8b1e9d2d9b19
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-25 23:54:39 +0700

    Add global state & event handlers for Menu component

--- src/components/core/skeleton/Providers/Global/index.jsx
+++ src/components/core/skeleton/Providers/Global/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import exact from 'prop-types-exact';
 import React from 'react';
 
-const initialState = {};
+const initialState = { isMenu: false };
 const Context = React.createContext({});
 
 const propTypes = exact({
@@ -12,9 +12,27 @@ const propTypes = exact({
 class Global extends React.Component {
   state = { ...initialState };
 
+  handleMenuChange = state => {
+    this.setState({ isMenu: state.isOpen });
+  };
+
+  handleMenuClose = () => {
+    this.setState({ isMenu: false });
+  };
+
+  handleMenuToggle = () => {
+    this.setState(state => ({ isMenu: !state.isMenu }));
+  };
+
   render() {
     const { children } = this.props;
-    const actions = {};
+    const actions = {
+      menu: {
+        onChange: this.handleMenuChange,
+        onClose: this.handleMenuClose,
+        onToggle: this.handleMenuToggle
+      }
+    };
     const { state } = this;
 
     return (
