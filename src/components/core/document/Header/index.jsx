163b3fd51e1afa01205b50d44e08e3f97bc82f67
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-26 00:06:27 +0700

    Refactor Header components
    - Updage content structure
    - Updatae styles
    - Include Burger & GitHub components
    - Add snapshot tests

--- src/components/core/document/Header/index.jsx
+++ src/components/core/document/Header/index.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Grid from 'components/common/base/Grid';
+import Brand from 'components/common/base/Brand';
+import Burger from 'components/common/base/Burger';
+import GitHub from 'components/common/composite/GitHub';
+import withContext from 'HOCs/withContext';
+
+const Content = styled.div`
+  align-items: center;
+  display: flex;
+  height: 30px;
+  justify-content: space-between;
+  margin: 0.5rem 0;
+`;
 
 const Frame = styled.header`
   background-color: #fff;
@@ -14,24 +26,42 @@ const Frame = styled.header`
   right: 0;
   top: 0;
   width: 100%;
-  z-index: 1;
+  z-index: 2;
 `;
 
-const Content = styled.div`
+const Meta = styled.div`
   align-items: center;
   display: flex;
-  height: 30px;
-  justify-content: space-between;
-  margin: 0.5rem 0;
 `;
 
-function Header() {
+const Nav = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+const Toggle = styled(Burger)`
+  margin-left: -0.65rem;
+  margin-right: 0.75rem;
+`;
+
+function Header({ context }) {
+  const { actions } = context;
+  const { onToggle } = actions.menu;
+
   return (
     <Frame>
       <Grid.Container>
         <Grid.Row>
           <Grid.Column>
-            <Content>Header</Content>
+            <Content>
+              <Nav>
+                <Toggle onToggle={onToggle} />
+                <Brand />
+              </Nav>
+              <Meta>
+                <GitHub />
+              </Meta>
+            </Content>
           </Grid.Column>
         </Grid.Row>
       </Grid.Container>
@@ -39,4 +69,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default withContext(Header);
