f36ca1f3cca5ebb89d22d2fc50cbe9b154617d25
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-26 16:05:35 +0700

    Include Gallery component in Home screen

--- src/screens/main/Home/Home.stories.jsx
+++ src/screens/main/Home/Home.stories.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import Main from 'components/core/sections/Main';
+import Wrapper from 'stories/decorators/Wrapper';
 import Home from './index';
 
 const title = 'Screens/Main/Home';
 const component = <Home />;
 
-storiesOf(title, module).add('default', () => component);
+storiesOf(title, module)
+  .addDecorator(story => (
+    <Wrapper>
+      <Main>{story()}</Main>
+    </Wrapper>
+  ))
+  .add('default', () => component);
