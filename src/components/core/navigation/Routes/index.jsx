ca1a5027b9062134be969a2211b94c1fd6a4711e
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-27 12:08:16 +0700

    Add route for Terms screen

--- src/components/core/navigation/Routes/index.jsx
+++ src/components/core/navigation/Routes/index.jsx
@@ -7,12 +7,14 @@ import { Home } from 'screens';
 
 const NotFound = withLoadable(() => import('screens/main/NotFound'));
 const About = withLoadable(() => import('screens/static/About'));
+const Terms = withLoadable(() => import('screens/static/Terms'));
 
 function Routes() {
   return (
     <Switch>
       <Route component={About} path={PATHS.about} />
       <Route component={Home} exact path={PATHS.root} />
+      <Route component={Terms} path={PATHS.terms} />
       <Route component={NotFound} />
     </Switch>
   );
