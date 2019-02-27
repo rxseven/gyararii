6865c4efab1faef43a4bcac35338ac04760e4ff8
ME (Mac mini) <rxseven.com@gmail.com>
2019-02-27 12:07:52 +0700

    Add route for About screen

--- src/components/core/navigation/Routes/index.jsx
+++ src/components/core/navigation/Routes/index.jsx
@@ -6,10 +6,12 @@ import PATHS from 'constants/routes/paths';
 import { Home } from 'screens';
 
 const NotFound = withLoadable(() => import('screens/main/NotFound'));
+const About = withLoadable(() => import('screens/static/About'));
 
 function Routes() {
   return (
     <Switch>
+      <Route component={About} path={PATHS.about} />
       <Route component={Home} exact path={PATHS.root} />
       <Route component={NotFound} />
     </Switch>
